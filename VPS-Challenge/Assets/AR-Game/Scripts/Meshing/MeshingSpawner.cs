593057c81ddf127536440aa9d2675ee9f9023bed
CamiloMGS <93027900+CamiloMGS@users.noreply.github.com>
2024-06-23 16:41:08 +0200

    Floor Finder

--- VPS-Challenge/Assets/AR-Game/Scripts/Meshing/MeshingSpawner.cs
+++ VPS-Challenge/Assets/AR-Game/Scripts/Meshing/MeshingSpawner.cs
@@ -10,31 +10,44 @@ using System;
 
 public class MeshingSpawner : MonoBehaviour
 {
-    [Header("AR")]
+    [Header("AR References")]
     [SerializeField] private XROrigin origin;
     [SerializeField] private ARMeshManager meshManager;
     [SerializeField] private GameObject meshRoot;
+    [SerializeField] GameObject SnakeManagerGO;
 
-    [Header("Mesh Param")]
+    [Header("Mesh Scan Parameters")]
     [SerializeField] private int minimumMeshBlocks = 30;
     [SerializeField] private int miniumTimeMesh = 5000;
+
     [Header("Object Generation Parameters")]
     [SerializeField] private float groundTolerance = 0.05f;
     [SerializeField] private float centerOffset = 0.15f;
     [SerializeField] private float appleOffset = 0.015f;
     [SerializeField] private float minDisEnemies = 0.4f;
 
+    [SerializeField] private float snakeY;
+    [SerializeField] private float yOffset = 0.05f;
+
+    public List<GameObject> FloorMeshes = new List<GameObject>();
+    [SerializeField] private float lowerLimit;
+    [SerializeField] private float upperLimit;
+
+
     private Stopwatch meshTimer = new Stopwatch();
     private bool isTimerComplete;
     private bool isMinimulBlocks;
-
-    [SerializeField]private List<GameObject> meshBloks = new List<GameObject>();
+    private List<GameObject> meshBloks = new List<GameObject>();
     private Dictionary<GameObject, Vector3> BlockCenterDic = new Dictionary<GameObject, Vector3>();
+    private List<float> meshCenters;
+
     // Start is called before the first frame update
     void Start()
     {
         meshManager.meshesChanged += MeshesChanged;
         meshRoot = origin.TrackablesParent.gameObject;
+
+        GameManager.Instance.OnSnakePlaced += FindFloor;
     }
 
     private void MeshesChanged(ARMeshesChangedEventArgs obj)
@@ -89,6 +102,47 @@ public class MeshingSpawner : MonoBehaviour
         else
         {
             BlockCenterDic[child] = meshCenter;
+        }
+    }
+
+
+
+    private void FindFloor()
+    {
+        meshCenters = new List<float>();
+        snakeY = SnakeManagerGO.transform.position.y + yOffset;
+        lowerLimit = snakeY - centerOffset;
+        upperLimit = snakeY + centerOffset;
+
+        //upper.position = new Vector3(SnakeManagerGO.transform.position.x, upperLimit, SnakeManagerGO.transform.position.y);
+        //lower.position = new Vector3(SnakeManagerGO.transform.position.x, lowerLimit, SnakeManagerGO.transform.position.y);
+
+        foreach (var pair in BlockCenterDic)
+        {
+            var key = pair.Key;
+
+            if (key == null)
+            {
+                continue;
+            }
+
+            Vector3 centerPos = pair.Value;
+
+            if (lowerLimit <= centerPos.y && centerPos.y <= upperLimit)
+            {
+                if (!FloorMeshes.Contains(key))
+                {
+                    meshCenters.Add(centerPos.y);
+                    FloorMeshes.Add(key);
+                }
+            }
+            else
+            {
+                if (FloorMeshes.Contains(key))
+                {
+                    FloorMeshes.Remove(key);
+                }
+            }
         }
     }
 }
--- VPS-Challenge/Assets/AR-Game/Scripts/Snake/SnakeManager.cs
+++ VPS-Challenge/Assets/AR-Game/Scripts/Snake/SnakeManager.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class SnakeManager : MonoBehaviour
 {
+
+    public static SnakeManager Instance;
+
+
+    public Mesh currentMesh;
+    public GameObject currenMeshContainer;
+    public bool justSpawned = true;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +33,21 @@ public class SnakeManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        var meshFilter = collision.gameObject.GetComponent<MeshFilter>();
+
+        if (meshFilter)
+        {
+            currentMesh = meshFilter.sharedMesh;
+            currenMeshContainer = collision.gameObject;
+            if (justSpawned)
+            {
+                GameManager.Instance.SnakePlaced();
+                justSpawned = false;
+            }
+        }
     }
 }
